9e3def05ef28e77201a10bc9f25fb7f85172a196
Taylor Finklea <taylor.finklea@gmail.com>
2023-02-10 06:57:58 -0600

    working on about page

---
+++ components/SocialLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  icon: string
+  label: string
+  href: string
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <li class="mb-3">
+    <NuxtLink
+      class="group flex text-sm font-medium text-primary-dark-800 transition hover:text-primary-500 dark:text-primary-light-300"
+      :to="props.href"
+    >
+      <Icon class="w-6 h-6 text-primary-light-600 dark:text-primary-light-400" :name="props.icon" /><span class="pl-5">{{ props.label }}</span>
+    </NuxtLink>
+  </li>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,26 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const social = [
+  {
+    label: 'GitHub',
+    icon: 'mdi:github',
+    href: 'https://github.com/TaylorFinklea',
+  },
+  {
+    label: 'LinkedIn',
+    icon: 'mdi:linkedin',
+    href: 'https://www.linkedin.com/in/taylor-finklea-8614238b',
+  },
+  {
+    label: 'Unsplash',
+    icon: 'ri:unsplash-fill',
+    href: 'https://unsplash.com/@tfinklea',
+  },
+  {
+    label: 'Thingiverse',
+    icon: 'simple-icons:thingiverse',
+    href: 'https://www.thingiverse.com/taylorfinklea/designs',
+  },
+]
+</script>
 <template>
   <div
-    class="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto-1fr] lg:gap-y-12 bg-blue-600"
+    class="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto-1fr] lg:gap-y-0"
   >
-    <div class="lg:pl-20 bg-green-500">
-      <div class="max-w-xs px-2.5 lg:max-w-none bg-red-500">
-        <NuxtImg src="/test01.jpeg" sizes="sm:100vw md:50vw lg:300px" />
+    <div class="lg:pl-20">
+      <div class="max-w-xs px-2.5 lg:max-w-none">
+        <NuxtImg
+          src="/taylor2.jpg"
+          sizes="sm:100vw md:50vw lg:300px"
+          class="rounded-3xl"
+        />
       </div>
     </div>
-    <div class="lg:order-first lg:row-span-2 bg-orange-500">
+    <div class="lg:order-first lg:row-span-2">
       <h1
-        class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
+        class="text-4xl font-bold tracking-tight text-primar-dark-800 dark:text-primary-light-100 sm:text-5xl"
       >
-        Hi, I'm Taylor Finklea. I am a maker and an automator.
+        Hi, I'm Taylor Finklea.
+      </h1>
+      <h1
+        class="text-4xl font-bold tracking-tight text-primary-dark-800 dark:text-primary-light-100 sm:text-5xl"
+      >
+        I am a maker and an automator.
       </h1>
       <div
-        class="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400 bg-amber-500"
+        class="mt-6 space-y-4 text-base text-primary-dark-600 dark:text-primary-light-300"
       >
         <p>
           I have always had a passion for building and automating things. As a
           child I was constantly tearing apart electronics to figure out how
-          they worked. In 2017 I build my first Prusa i3 3D printer and taught
+          they worked. In 2017 I built my first Prusa i3 3D printer and taught
           myself Autodesk Fusion 360 so that I could model my own designs.
         </p>
         <p>
@@ -32,30 +64,34 @@
           WLED.
         </p>
         <p>
-          Despite my many technical interests, I love to be outside. I am an
-          amateur wildlife and nature photographer and especially enjoy
+          In addition to my many technical interests, I love to be outside. I am
+          an amateur wildlife and nature photographer and especially enjoy
           photographing and learning about birds. I strive to hike daily where
-          I'm able to disconnect and ground myself.
+          I'm able to connect with nature and ground myself.
         </p>
         <p>
-          My wife, kids and I enjoy spending time together and trying new
-          things. Together we garden, tend to our land and animals and explore
-          our many acres here in Kansas City.
+          My wife, kids and I enjoy spending time together, eating good food and
+          trying new things. Together we garden, tend to our land and animals
+          and explore the area here in Kansas City.
         </p>
       </div>
     </div>
-    <div class="lg:pl-20 bg-purple-500">
+    <div class="lg:pl-20">
       <ul>
-        <li>LinkedIn</li>
-        <li>Github</li>
-        <li>Unsplash</li>
-        <li>Thingiverse</li>
+        <SocialLink
+          v-for="link in social"
+          :href="link.href"
+          :label="link.label"
+          :icon="link.icon"
+        />
         <li
           class="mt-8 border-t border-primary-dark-100 pt-8 dark:border-primary-dark-700/40"
         >
-          <NuxtLink to="mailto:taylor.finklea@gmail.com">
-            taylor.finklea@gmail.com
-          </NuxtLink>
+          <SocialLink
+            href="mailto:taylor.finklea@gmail.com"
+            label="taylor.finklea@gmail.com"
+            icon="mdi:email"
+          ></SocialLink>
         </li>
       </ul>
     </div>
