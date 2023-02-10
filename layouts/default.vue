5b59c8948f331919089b2474aafeca2e715f0b27
Taylor Finklea <taylor.finklea@gmail.com>
2023-02-10 13:48:28 -0600

    darker background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div className="inset-0 flex justify-center sm:px-12 dark:bg-black">
+  <div class="inset-0 flex justify-center sm:px-12 dark:bg-black">
     <div
-      className="w-full bg-white ring-1 ring-primary-light-100 dark:bg-primary-dark-800 dark:ring-primary-dark-300/20 max-w-screen-lg"
+      class="w-full bg-white ring-1 ring-primary-light-100 dark:bg-primary-dark-900 dark:ring-primary-dark-300/20 max-w-screen-lg"
     >
       <NavBar />
       <div
-        class="p-12 bg-white dark:bg-primary-dark-800 w-full h-full min-h-screen flex flex-col items-center"
+        class="p-12 bg-white dark:bg-primary-dark-900 w-full h-full min-h-screen flex flex-col items-center"
       >
         <slot />
       </div>
