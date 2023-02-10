4108ccbefc1f6f5cc5d98eaf160a2b87239f51a4
Taylor Finklea <taylor.finklea@gmail.com>
2023-02-10 13:51:03 -0600

    Fixed wrapping div

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="inset-0 flex justify-center sm:px-12 dark:bg-black">
     <div
-      class="w-full bg-white ring-1 ring-primary-light-100 dark:bg-primary-dark-900 dark:ring-primary-dark-300/20 max-w-screen-lg"
+      class="w-full bg-white ring-1 ring-primary-light-100 dark:bg-primary-dark-900 dark:ring-primary-dark-300/20 max-w-screen-lg h-full"
     >
       <NavBar />
       <div
