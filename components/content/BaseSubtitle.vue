c4309708a3d52403514473d25ec8e99151edef60
Taylor Finklea <taylor.finklea@gmail.com>
2023-02-15 06:52:40 -0600

    remove legacy components

--- components/content/BaseSubtitle.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="mt-6 mb-10 space-y-4 text-base text-primary-dark-600 dark:text-primary-light-300"
-  >
-    <slot />
-  </div>
-</template>
--- components/content/BaseTitle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="text-4xl font-bold tracking-tight text-primary-dark-800 dark:text-primary-light-100 sm:text-5xl">
-    <slot />
-  </div>
-</template>
